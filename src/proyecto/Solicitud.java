50fee73a4b8def6dd2236408c38e6ffb329b78ce
diego <diego@DESKTOP-7T100KB>
2022-12-02 22:52:40 -0600

    Agregar clase Solicitud

---
+++ src/proyecto/Solicitud.java
@@ -0,0 +1,100 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto;
+
+/**
+ *
+ * @author diego
+ */
+public class Solicitud {
+    //Atributos de la solicitud
+    private int cedula;
+    private String fecha;
+   // private String placa;
+    private int dias;
+    private String estado;
+    private int pasajeros;
+    private String marca;
+    private String modelo;
+    private String extras;
+    
+    //Constructor
+    public Solicitud(int cedula,String fecha,int dias,String estado,
+            int pasajeros,String marca,String modelo,String extras){
+        this.cedula = cedula;
+        this.fecha = fecha;
+        this.dias = dias;
+        this.estado = estado;
+        this.pasajeros = pasajeros;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.extras = extras;
+    }
+    
+    //Setters and Getters
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public int getDias() {
+        return dias;
+    }
+
+    public void setDias(int dias) {
+        this.dias = dias;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getPasajeros() {
+        return pasajeros;
+    }
+
+    public void setPasajeros(int pasajeros) {
+        this.pasajeros = pasajeros;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getExtras() {
+        return extras;
+    }
+
+    public void setExtras(String extras) {
+        this.extras = extras;
+    } 
+}
