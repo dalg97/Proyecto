78c572a1bb5cf5e90826272e93edd40c8bca45e9
diego <diego@DESKTOP-7T100KB>
2022-12-14 18:44:28 -0600

    Eliminando espacios

--- src/proyecto/Lista.java
+++ src/proyecto/Lista.java
@@ -9,41 +9,35 @@ import javax.swing.JOptionPane;
  * @author danny
  */
 public class Lista {
-
+    //Atributos de la lista
     private Nodo2 cabeza;
-    //private Nodo2 ultimo;
     private String array [] = new String [11];
-
+    
+    //Metodo para insertar vehiculos
     public void insertar(Vehiculo vehiculo) {
         if (cabeza == null) {
             //si la lista esta vacia, se inserta como la cabeza
             cabeza = new Nodo2(vehiculo);
-           // ultimo = cabeza;
-            //si no esta vacia, verifica si el id es menor al id de la cabeza
+            //si no esta vacia, verifica si el anio es mas reciente
         } else if (vehiculo.getAnio() > cabeza.getVehiculo().getAnio()) {
-            //si es mayor, crea el nodo temporal y lo asigna a la izquierda
+            //si es reciente, crea el nodo temporal y lo asigna a la izquierda
             //de la cabeza
             Nodo2 aux = new Nodo2(vehiculo);
             aux.setNext(cabeza);
             cabeza.setAtras(aux);
             //cabeza toma el valor del nodo temporal
             cabeza = aux;
-            //si el siguiente nodo de la cabeza es nulo, se agrega el nuevo
-            //nodo
         } else if (cabeza.getNext() == null) {
+            //Si el siguiente es null, se pone al lado derecho
             Nodo2 aux = new Nodo2(vehiculo);
-            //aux.setNext(cabeza.getNext());
-            //aux.setAtras(cabeza);
-           // cabeza.getAtras().setAtras(aux);
             cabeza.setNext(aux);
             aux.setAtras(cabeza);
         } else {
             //si no cumple con las anteriores, se crea el nodo temporal con 
             //el valor de la cabeza e ingresa al ciclo para recorrer la lista
             Nodo2 aux = cabeza;
-
-            //si el id del siguiente nodo es mayor al id del nuevo nodo, este 
-            //pasaria a ser el siguiente nodo
+            //Ingresa a un while para recorrer la lista y verificar si el anio 
+            //es mas reciente para colocarlo a la derecha
             while (aux.getNext() != null
                     && aux.getNext().getVehiculo().getAnio()
                     < vehiculo.getAnio()) {
@@ -57,66 +51,48 @@ public class Lista {
             temp.setAtras(aux);
         }
     }
-
+    //metodo modificar vehiculo
     public void modificar(Vehiculo vehiculo) {
-        //Buscar el ID y ver si este existe
+        //Buscar el vehiculo y ver si este existe
         if (cabeza != null) {
-            //Si hay algo en la lista buscaré
-            Nodo2 aux = cabeza; //utilizo aux como un buscador
-            //La lista no puede estar vacía para poder obtener los 
-            //datos exitentes y verificar que estos sean  menores al ID 
+            //buscar si no esta vacia
+            Nodo2 aux = cabeza; //utilizo aux como un buscador            
             while (aux != null && aux.getVehiculo().getAnio()
                     < vehiculo.getAnio()) {
                 aux = aux.getNext(); //Continuar con la busqueda
-            }
-            // Si el aux es diferente a nulo y se logran obtener los datos
-            //siempre y cuando el ID exista se va a proceder a modificar el 
-            //nombre
+            }           
             if (aux != null && aux.getVehiculo().getAnio()
                     == vehiculo.getAnio()) {
-                //Se obtienen los datos para cambiar el valor del nombre
+                //Se obtienen los datos para cambiar el valor del estado
                 aux.getVehiculo().setEstado("Alquilado");
             }
         }
     }
-
-    public void eliminar(String placa) { //definimos el metodo llamado elimina y 
-        //ponemos como parametro el int id 
-        //Lo que vamos a hacer es eliminar el id de una persona
-        System.out.println(placa);
+    //metodo eliminar vehiculo
+    public void eliminar(String placa) { 
+        //ponemos como parametro la placa 
         if (cabeza != null) {//Si la lista no esta nula entonces empieza a buscar
-            System.out.println(cabeza.getVehiculo().getPlaca());
             if (cabeza.getVehiculo().getPlaca().equals(placa)) {
-                //buscamos el id utilizando un 
-                //if statement y verificamos que la cabeza cumpla el parametro del id
+                //si es la placa hace match con la cabeza                  
                 if(cabeza.getNext() == null){
                     cabeza = null;
                 }else{
                 cabeza = cabeza.getNext();
-                }//sino cumple como parametro, 
-                //seguimos buscando y definimos el siguiente nodo como cabeza
+                }
             } else {
+                //Si no es la cabeza, recorre el la lista para encontrarlo
                 Nodo2 aux = cabeza; //se utiliza el modo auxiliar 
-                //Mientras el nodo no este vacío 
-                //de la lista sea menor que el buscado
                 while (aux.getNext() != null
                         && !aux.getNext().getVehiculo().getPlaca().equals(placa)) {
-                    aux = aux.getNext();
-                    System.out.println("Entro al while");
+                    aux = aux.getNext();//avanzo en la lista
                 }
-                //avanzo en la lista
 
-                // si el que le sigue es mayor entonces para el ciclo 
-                //y se ejecuta lo siguiente
                 if (aux.getNext() != null
               && aux.getNext().getVehiculo().getPlaca().equals(placa)) {
-                    //si el id es igual al del parametro...
+                    //si la placa es igual al del parametro...
                     if(aux.getNext().getNext() == null){
-                        aux.setNext(null);
-                        System.out.println("quitar el ultimo");
-          
+                        aux.setNext(null);         
                     }else{
-                        System.out.println("quitar el del medio");
                         aux.setNext(aux.getNext().getNext());
                         aux.getNext().getNext().setAtras(aux);
                     }
@@ -124,9 +100,8 @@ public class Lista {
                 }
             }
         }
-
     }
-    
+    //Metodo mostrar la lista
     public void mostrarVehiculos() {
         Nodo2 aux = cabeza;
         String output = "";
@@ -148,15 +123,14 @@ public class Lista {
         }
         JOptionPane.showMessageDialog(null, output);
     }
-    
+    //metodo para verificar si existe el vehiculo
     public boolean existe(String placa) {
         boolean exist = false;
         //verificar si la lista esta vacia
         if (cabeza != null) {
             //sino, se crea el nodo temporal para recorrer la lista
             Nodo2 aux = cabeza;
-            //ingresa a un ciclo mientras el nodo no este null y el
-            //id del nodo sea menor que el parametro
+            //ingresa a un ciclo mientras el nodo no este null
             while (aux != null && !aux.getVehiculo().getPlaca().equals
                 (placa) ) {
                 aux = aux.getNext();
@@ -167,16 +141,18 @@ public class Lista {
     }
         return exist;
   }
+    //metodo para devolver el array con los attributos
     public String[] getArray() {
         return array;
 }
+    //MEtodo modificar vehiculo
     public void modificarV(String Placa, String marca, String modelo, int anio,
             String color, int cilindrada, String combustible, int pasajeros,
             int precio, String Estado, String Extras){
         Nodo2 aux=cabeza;
         while (aux != null) {
+            //Si la encuentra la placa, cambia valores
             if(Placa.equals(aux.getVehiculo().getPlaca())){
-                System.out.println("Entro al if");
                 aux.getVehiculo().setPlaca(Placa);
                 aux.getVehiculo().setColor(color);
                 aux.getVehiculo().setExtras(Extras);
@@ -194,15 +170,15 @@ public class Lista {
             }
         }  
     }
+    //Metodo para obtener los atributos ya seteados
     public void getAtributos(String placa){
         Nodo2 aux = cabeza;
-        // Bandera para verificar si exist el elemento a search.
         // Recorre la pila hasta llegar encontrar el node o llegar al final
-        // de la pila.
+        // de la lista
         while (aux != null) {
-            // Compara si el value del node es igual que al de reference.
+            // Compara si el placa es igual al de reference.
             if (placa.equals(aux.getVehiculo().getPlaca())) {
-                // Cambia el value de la bandera.
+                //Almacenar los atributos en el array
                 array[0] = aux.getVehiculo().getPlaca();
                 array[1] = aux.getVehiculo().getColor();
                 array[2] = aux.getVehiculo().getExtras();
@@ -213,16 +189,14 @@ public class Lista {
                 array[7] = (String.valueOf(aux.getVehiculo().getCilidrada()));
                 array[8] = (String.valueOf(aux.getVehiculo().getPrecio()));
                 array[9] = (String.valueOf(aux.getVehiculo().getAnio()));
-                array[10] = (aux.getVehiculo().getModelo());
-                                
+                array[10] = (aux.getVehiculo().getModelo());                               
                 aux = null;
             } else {
                 // Avanza al siguiente node.
                 aux = aux.getNext();
             }    
         }        
-   }
-     
+   }    
 }
     
         
